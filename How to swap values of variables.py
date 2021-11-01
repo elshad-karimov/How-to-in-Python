2bc78e0425be5243843417c3abe08acd09559794
Elshad Karimov <32020767+elshad-karimov@users.noreply.github.com>
2021-11-01 19:01:29 +0000

    initial

---
+++ How to swap values of variables.py
@@ -0,0 +1,16 @@
+# How to swap values of variables in Python?
+
+# Let's say we want to swap the values 
+# of x and y...
+x = 10
+y = 20
+
+# The "classic" way to do it with a temporary 
+# variable:
+tmp = x
+x = y
+y = tmp
+
+# Python also lets us use this short-hand:
+x, y = y, x
+
