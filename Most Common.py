645968436b107e8d0495c67269d2ac337651fcfe
Elshad Karimov <32020767+elshad-karimov@users.noreply.github.com>
2021-12-13 19:01:45 +0000

    Most common added

---
+++ Most Common.py
@@ -0,0 +1,21 @@
+# collections.Counter lets you find the most common
+# elements in an iterable:
+
+import collections
+c = collections.Counter('appmillers.com')
+
+print(c.most_common(4))
+# [('p', 2), ('m', 2), ('l', 2), ('a', 1)]
+
+
+
+
+
+
+
+
+
+
+
+
+
